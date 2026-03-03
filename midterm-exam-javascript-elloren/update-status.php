9efcce6cfe0eff489800fceb6c1a168817f0dd6a
adelloren <adelloren.student@asiancollege.edu.ph>
2026-03-03 09:19:16 +0800

    Add files via upload
    Members:
    Arrianna Joy D. Elloren
    Stephanie Marie T. Quinillo
    Urvisa C. Tawing
    Cristine M. Santos

---
+++ midterm-exam-javascript-elloren/update-status.php
@@ -0,0 +1,15 @@
+<?php
+include "db.php";
+
+$id = $_POST['id'];
+$status = $_POST['status'];
+
+
+if (!in_array($status, ['pending', 'done'])) {
+    $status = 'pending';
+}
+
+$conn->query("UPDATE tasks SET status='$status' WHERE id=$id");
+
+echo json_encode(['success' => true]);
+?>
