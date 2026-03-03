4e7d4c6fe5cb77a5cf22da2b6985196ddae4b7f3
adelloren <adelloren.student@asiancollege.edu.ph>
2026-03-03 11:45:19 +0800

    Delete yhannaDelux directory

--- yhannaDelux/add.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "db.php";
-
-$task = $_POST['task'];
-
-$conn->query("INSERT INTO tasks(task_name, status) VALUES('$task', 'pending')");
-
-header("Location: index.php");
-?>
--- yhannaDelux/cosa.js
+++
@@ -1,9 +0,0 @@
-function editTask(id) {
-
-    let oldText = document.getElementById("task-" + id).innerText;
-    let newTask = prompt("Edit task:", oldText);
-
-    if(newTask !== null && newTask.trim() !== "") {
-        window.location = "update.php?id=" + id + "&task=" + encodeURIComponent(newTask);
-    }
-}
